3cedca25afa6f1dc1ed650600aa545f45b6d45fb
bronson113 <bronson1013@gmail.com>
2024-03-08 16:49:23 +0800

    Dark mode ACTIVATED!
    add dark mode

--- js/main.js
+++ js/main.js
@@ -12,7 +12,31 @@ function toggleScrollToTopButton(){
   }
 }
 
+// setup color-mode button
+$('.color-mode').click(function(){
+  if(localStorage.getItem('dark') === "true"){
+    localStorage.setItem('dark', "false");
+  } else {
+    localStorage.setItem('dark', "true");
+  }
+    $('.color-mode-light').toggleClass('d-none')
+    $('.color-mode-dark').toggleClass('d-none')
+    $('body').toggleClass('dark-mode')
+    $('body').attr("data-bs-theme", localStorage.getItem('dark') === "true" ? "dark":"light")
+})
+
+
+if(localStorage.getItem('dark') === "true"){
+  $('body').toggleClass('dark-mode')
+  $('.color-mode-light').toggleClass('d-none')
+  $('.color-mode-dark').toggleClass('d-none')
+}
+$('body').attr("data-bs-theme", localStorage.getItem('dark') === "true" ? "dark":"light")
+
+// initialize scroll button
 $(document).ready(function () {
   $('#back-to-top-button').click(scrollToTop);
   $(window).scroll(toggleScrollToTopButton);
 })
+
+
